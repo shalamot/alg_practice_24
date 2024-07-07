8250599f106961295305619b731f149ac4bc3211
vRedniy-LumbeRt <112636677+vRedniy-LumbeRt@users.noreply.github.com>
2024-07-07 09:20:03 +0300

    Update ideal_test_3.py
    соответственно, исправил идеальные тесты

--- Test_module/ideal_moodle_tests/ideal_test_3.py
+++ Test_module/ideal_moodle_tests/ideal_test_3.py
@@ -1,13 +1,10 @@
 import pytest
 
 def test_1():
-    assert foo(0) == False
-
-def test_2():
     assert foo(-2) == False
 
-def test_3():
+def test_2():
     assert foo(100) == False
 
-def test_4():
+def test_3():
     assert foo(4) == True
