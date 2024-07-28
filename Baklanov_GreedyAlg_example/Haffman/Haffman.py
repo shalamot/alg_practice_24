9e6f1fffec5528a0a51fed09bf63cc10c838f7aa
Fraerus <112628437+Oleg24042004@users.noreply.github.com>
2024-07-28 16:49:45 +0300

    Update Haffman.py

--- Baklanov_GreedyAlg_example/Haffman/Haffman.py
+++ Baklanov_GreedyAlg_example/Haffman/Haffman.py
@@ -1,61 +1,61 @@
-import heapq
+class NodeTree(object):
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
 
-class Node:
-    def __init__(self, symbol=None, frequency=None):
-        self.symbol = symbol
-        self.frequency = frequency
-        self.left = None
-        self.right = None
+    def children(self):
+        return (self.left, self.right)
 
-    def __lt__(self, other):
-        return self.frequency < other.frequency
+    def nodes(self):
+        return (self.left, self.right)
 
-def build_huffman_tree(chars, freq):
-    priority_queue = [Node(char, f) for char, f in zip(chars, freq)]
-    heapq.heapify(priority_queue)
-    while len(priority_queue) > 1:
-        left_child = heapq.heappop(priority_queue)
-        right_child = heapq.heappop(priority_queue)
-        merged_node = Node(frequency=left_child.frequency + right_child.frequency)
-        merged_node.left = left_child
-        merged_node.right = right_child
-        heapq.heappush(priority_queue, merged_node)
-    return priority_queue[0]
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
 
-def generate_huffman_codes(node, code="", huffman_codes={}):
-    if node is not None:
-        if node.symbol is not None:
-            huffman_codes[node.symbol] = code
-        generate_huffman_codes(node.left, code + "0", huffman_codes)
-        generate_huffman_codes(node.right, code + "1", huffman_codes)
 
-    return huffman_codes
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
 
-def huffman_encode(text, huffman_codes):
-    return ''.join(huffman_codes[symbol] for symbol in text)
 
-def huffman_decode(encoded_text, root):
-    decoded_text = []
-    node = root
-    for bit in encoded_text:
-        node = node.left if bit == '0' else node.right
-        if node.symbol:
-            decoded_text.append(node.symbol)
-            node = root
-    return ''.join(decoded_text)
+def encode_string(string, huffman_code):
+    return ''.join([huffman_code[char] for char in string])
 
-if __name__ == "__main__":
-    dna_sequence = input()
-    freq = {}
-    for char in dna_sequence:
-        if char in freq:
-            freq[char] += 1
-        else:
-            freq[char] = 1
-    chars = list(freq.keys())
-    frequencies = list(freq.values())
-    huffman_tree = build_huffman_tree(chars, frequencies)
-    huffman_codes = generate_huffman_codes(huffman_tree)
-    encoded_text = huffman_encode(dna_sequence, huffman_codes)
-    decoded_text = huffman_decode(encoded_text, huffman_tree)
-    print(encoded_text, decoded_text == dna_sequence)
+
+# Input string
+string = input()
+
+# Calculating frequency
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+# Sorting frequency dictionary into a list of tuples
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+# Creating nodes
+nodes = freq
+
+# Building the Huffman tree
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+# Generating Huffman codes
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+# Encoding the string
+encoded_string = encode_string(string, huffmanCode)
+print(encoded_string)
